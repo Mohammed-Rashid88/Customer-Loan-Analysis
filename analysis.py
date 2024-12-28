5303245f48927c8347aa8190503ab71b8fc6d165
Mohammed-Rashid88 <mtr96@drexel.edu>
2024-12-28 14:35:35 -0500

    Create analysis.py

---
+++ analysis.py
@@ -0,0 +1,21 @@
+from sklearn.linear_model import LogisticRegression
+from sklearn import metrics
+import numpy as np
+import pandas as pd
+
+# load the dataset and prepare features
+data = pd.read_csv(r'C:\Users\rohan\Downloads\Task 3 and 4_Loan_Data.csv')
+attributes = ['credit_lines_outstanding', 'debt_to_income_ratio', 'payment_to_income_ratio', 'years_employed', 'fico_score']
+data['payment_to_income_ratio'] = data['loan_amt_outstanding'] / data['income']
+data['debt_to_income_ratio'] = data['total_debt_outstanding'] / data['income']
+
+# initialize and train the logistic regression model
+model = LogisticRegression(random_state=0, solver='liblinear', tol=1e-5, max_iter=10000)
+model.fit(data[attributes], data['default'])
+
+# calculate expected financial loss based on the trained model
+def calculate_expected_loss(data_row):
+    data_row['payment_to_income_ratio'] = data_row['loan_amt_outstanding'] / data_row['income']
+    data_row['debt_to_income_ratio'] = data_row['total_debt_outstanding'] / data_row['income']
+    probability_of_default = model.predict_proba(data_row[attributes].values.reshape(1, -1))
+    return probability_of_default[0, 1] * data_row['loan_amt_outstanding'] * 0.1
